9e125b52b0050124eeb795fe4c0c627e3936f6f7
Nattida Wong <nattidac.wong@gmail.com>
2020-10-21 21:46:43 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+
+class GetRequester
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    require 'open-uri'
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    uri = URI.parse(url)
+    uri.open.string
+  end
+
+  def parse_json
+    require 'open-uri'
+    require 'net/http'
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body)
+  end
+end
